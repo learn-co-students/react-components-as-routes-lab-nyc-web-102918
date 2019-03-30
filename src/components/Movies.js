7e1e8d4fd2e81f96fbd42f0cf7423aab3cbed236
Connor Callahan <connorwcallahan@gmail.com>
2019-03-30 13:30:46 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,18 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) =>
+          <div>
+            <p>Title: {movie.title}</p>
+            <p>Time: {movie.time}</p>
+            <ul>
+              {movie.genres.map((genre) =>
+                <li>{genre}</li>
+              )}
+            </ul>
+          </div>
+        )}
     </div>
   );
 };
